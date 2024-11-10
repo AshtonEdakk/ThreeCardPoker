98b9a8309970bf36471a706b2dd9833693c13b8e
AshtonEdakk <aedak2@uic.edu>
2024-11-09 22:08:29 -0600

    back up cu im about to do something that will either fix my code or completly break it ;-;

--- JavaFX_MavenTemplate_VS1 2/src/main/java/Card.java
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/Card.java
@@ -1,8 +1,7 @@
 public class Card {
     char suit;
     int value;
-    int hshshshs;
-
+    
     // Constructor
     public Card(char suit, int value) {
         this.suit = suit;
--- JavaFX_MavenTemplate_VS1 2/src/main/java/welcomeController.java
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/welcomeController.java
@@ -1,1 +1,18 @@
 //controller for the welcome screen
+import java.io.IOException;
+
+public class welcomeController implements Initializable{
+	
+	@FXML
+	private VBox root;
+	
+	@FXML
+	private BorderPane root2;
+	
+	@FXML
+	private TextField textField;
+	
+	@FXML
+	private TextField putText;
+	
+}
