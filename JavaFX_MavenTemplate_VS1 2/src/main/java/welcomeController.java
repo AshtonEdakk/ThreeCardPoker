ef991bbe20c84f865fc5db740c04d043b06c2e4c
AshtonEdakk <aedak2@uic.edu>
2024-11-10 18:44:28 -0600

    i did smt

--- JavaFX_MavenTemplate_VS1 2/src/main/java/welcomeController.java
+++ JavaFX_MavenTemplate_VS1 2/src/main/java/welcomeController.java
@@ -2,9 +2,7 @@
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.application.Application;
-
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
